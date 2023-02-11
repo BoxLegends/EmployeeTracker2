9877a43f18fc9ee22f27363fce20bd2e461e02e6
Nick Burns <nick_burns11@hotmail.com>
2023-02-11 00:05:19 -0500

    working on menu in server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const inquirer = require('inquirer');
+const db = mysql.createConnection(
+    {
+      host: "127.0.0.1",
+      user: 'root',
+      password: 'Nicholas11',
+      database: 'emp_db',
+    },
+    console.log(`Connected to the Employee database.`)
+  );
+
+
 
 function menu() {
   inquirer.prompt([
     {
     type: 'list',
     message: 'Select an option',
-    name: 'options',
+    name: 'option',
     choices: [
     
         "view all departments", 
@@ -17,6 +28,24 @@ function menu() {
         "update an employee role"
         ]
     }
+    .then(response =>{
+        if(response.option === "view all departments" ){
+            db.query("SELECT id, name as department_name from department ORDER BY name");
+            
+        }else if(response.option === "view all roles"){
+            console.log("view all roles");
+        }else if(response.option === "view all employees"){
+            console.log("view all emp");
+        }else if(response.option === "add a department"){
+            console.log("view all dept");
+        }else if(response.option === "add a role"){
+            console.log("add a role");
+        }else if(response.option === "add an employee"){
+            console.log("add an employee");
+        }//else (response.option === "update an employee role"){
+
+        //}
+    })
 ])}
 
 menu();
